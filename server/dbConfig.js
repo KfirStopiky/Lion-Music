2735b2ed1d81ef677ef4890cf050592a6cf19441
Kfir <stopiky2@gmail.com>
2022-02-15 22:50:26 +0200

    change port

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,13 +1,24 @@
 const mongoose = require("mongoose");
 
+// const connect_to_mongo_server = async () => {
+//   try {
+//     await mongoose.connect("mongodb://localhost/Shopping-Online", {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//     });
+//     console.log("MongoDB is connected");
+//   } catch (error) {
+//     console.log("Server Error");
+//   }
+// };
+
 const connect_to_mongo_server = async () => {
   try {
-    await mongoose.connect("mongodb://localhost/Shopping-Online", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    await mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://localhost/your-app-name"
+    );
     console.log("MongoDB is connected");
   } catch (error) {
     console.log("Server Error");
